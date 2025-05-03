68d23479a15aba4ee4a956fd2c1c3ccaa146db84
Ganesh Tiwari <ganesht049@gmail.com>
2025-05-03 18:58:46 +0530

    fix: added comments

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,13 +2,6 @@ locals {
   website_domain = "static-bucket.apps.gats.dev"
 }
 
-resource "google_compute_backend_bucket" "website-backend" {
-  name        = "gats-dev-static-content-backend"
-  description = "Contains a standard static website"
-  bucket_name = google_storage_bucket.website-bucket.name
-  enable_cdn  = true
-}
-
 resource "google_storage_bucket" "website-bucket" {
   name     = "gats-dev-static-content-bucket"
   location = var.region
@@ -19,13 +12,32 @@ resource "google_storage_bucket" "website-bucket" {
   }
 }
 
+
 resource "google_storage_bucket_iam_member" "allUsers" {
   bucket = google_storage_bucket.website-bucket.name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
 
-# Create url map
+# Backend Bucket
+# It creates a compute backend by using the storage bucket
+# as its source of static files
+# Normally you need a service to be added to a url-map,
+# this resources converts your Cloud Storage Blob storage
+# into an equivalent service that can be attached to a
+# Compute URL Map
+resource "google_compute_backend_bucket" "website-backend" {
+  name        = "gats-dev-static-content-backend"
+  description = "Contains a standard static website"
+  bucket_name = google_storage_bucket.website-bucket.name
+  enable_cdn  = true
+}
+
+# Create URL Map
+# This creates rules which can get attached to a proxy
+# according to which the proxy terminates the SSL connections
+# on it and then further down the lines passes those connections
+# to individual backend instances
 resource "google_compute_url_map" "default" {
   name = "http-lb"
 
@@ -43,11 +55,27 @@ resource "google_compute_url_map" "default" {
 }
 
 # Reserve IP address
+# This IP Address will be attached to the loadbalancer marking
+# as the entry point of traffic to it.
 resource "google_compute_global_address" "default" {
   name = "example-ip"
 }
 
+# GCP Managed SSL Certificate -
+# To be provision correctly it requires that the domain name
+# is pointed towards the IP address that we procured for the
+# website.
+resource "google_compute_managed_ssl_certificate" "default" {
+  name        = "static-bucket-cert"
+  description = "SSL certificate for static bucket"
+  managed {
+    domains = [local.website_domain]
+  }
+}
+
 # Create HTTP target proxy
+# This HTTPS Proxy is the actual thing which takes the rules
+# defined in url map and distribute traffic according to it.
 resource "google_compute_target_https_proxy" "default" {
   name    = "http-lb-proxy"
   url_map = google_compute_url_map.default.id
@@ -58,6 +86,8 @@ resource "google_compute_target_https_proxy" "default" {
 }
 
 # Create forwarding rule
+# Creating a global forwarding rule means that you are creating
+# the loadbalancer resource.
 resource "google_compute_global_forwarding_rule" "default" {
   name                  = "http-lb-forwarding-rule"
   ip_protocol           = "TCP"
@@ -65,12 +95,4 @@ resource "google_compute_global_forwarding_rule" "default" {
   port_range            = "443"
   target                = google_compute_target_https_proxy.default.id
   ip_address            = google_compute_global_address.default.id
-}
-
-resource "google_compute_managed_ssl_certificate" "default" {
-  name        = "static-bucket-cert"
-  description = "SSL certificate for static bucket"
-  managed {
-    domains = [local.website_domain]
-  }
 }
